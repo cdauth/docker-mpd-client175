94b75a63cea67186879ec8d66f9801ea15e21add
Candid Dauth <cdauth@cdauth.eu>
2015-11-09 17:10:11 +0100

    Show usage instructions in create.sh

--- create.sh
+++ create.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <container name>" >&2
+	exit 1
+fi
+
 docker rmi mpd_client175
 docker build --tag=mpd-client175 .
 docker create --device=/dev/snd:/dev/snd -p 6600:6600 -p 8002:8080 -v /share/share/Music:/music -v /run/dbus:/run/dbus --name="$1" mpd-client175
